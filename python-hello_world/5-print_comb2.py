56c60527be0e2d553cd8831cb81edfcd0500c771
Natnael Girma <ngirma700@gmail.com>
2023-07-15 22:39:16 +0300

    my first commit

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 0, 10):
-        print(f'{i}{j}', end=', ')
-print()
-
+for i in range(100):
+    if i == 99:
+        print(" {:02d}".format (i))
+        break
+    print ("{:02d}".format (i), end=", ")
