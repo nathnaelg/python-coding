b715713951140bab5b7efff9ba4e283348cb0e23
Natnael Girma <96662906+nathnaelg@users.noreply.github.com>
2023-07-17 20:10:31 +0300

    My first commit

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,3 @@
-for i in range(10):
-    for j in range(i + 1, 10):
-        print("{:02d}".format(i), "{:02d}".format(j), end=", " if i != 8 or j != 9 else "\n")
+for i in range (10):
+    for j in range (i + 1, 10):
+        print(" {:02d} ".format(i), "{:02d}".format (j), end=", " if i != 8 or j != 9 else "\n")
